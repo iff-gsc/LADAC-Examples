a8f9179b03dc2ea362b38c85d15b6cce3c24661e
Yannic Beyer <y.beyer@tu-bs.de>
2022-05-20 13:07:31 +0200

    Autopilot Dragonfly: clean up ArduCopter interface init

--- workbenches/Minnie/ArduPilot_implementation/init_Arducopter_MinnieDragonflyController.m
+++ workbenches/Minnie/ArduPilot_implementation/init_Arducopter_MinnieDragonflyController.m
@@ -1,9 +1,11 @@
-% Init Arducopter Interface for quadcopter Minnie with autopilot Dragonfly
+% init Arducopter Interface for quadcopter Minnie with autopilot Dragonfly
 
 % Disclamer:
 %   SPDX-License-Identifier: GPL-2.0-only
 % 
-%   Copyright (C) 2022 Yannic Beyer
+% 	Copyright (C) 2022 Yannic Beyer
+% 	Copyright (C) 2022 Fabian Guecker
+% 	Copyright (C) 2022 Eiker Bremers
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
@@ -12,19 +14,17 @@ cd ..
 addPathMinnie();
 clc_clear;
 
-%% waypoints
-waypoints = [ 0 0 0 1; 1 0 -1 1; 0 0 -2 1; -1 0 -1 1 ]'.*[10;10;10;14] + [0;0;-15;0];
-
 %% autopilot parameters
 ap_dragonfly = apCopterDragonflyLoadParams( 'apCopterDragonfly_params_Minnie' );
 
 %% initialize the Ardupilot Interface
 initInterfaceBuses
 
-%% Change the main struct from double to float
-Sample_Time = ap_dragonfly.sample_time;
+%% change the main struct from double to float
+sample_time = ap_dragonfly.sample_time;
 ap_dragonfly = structDouble2Single(ap_dragonfly);
-ap_dragonfly.sample_time = double(Sample_Time);
-clear Sample_Time;
+ap_dragonfly.sample_time = double(sample_time);
+clear sample_time;
 
+%% open Simulink model
 open ArduCopter_MinnieDragonflyController.slx
